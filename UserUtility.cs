b32b209cf53caed0cbd09dc9a0f79914df9c2caa
Abellegese <73869888+Abellegese@users.noreply.github.com>
2022-07-06 11:21:02 -0700

    Adding User Class
    This class can provide a data from the data to all other class which could use it.

---
+++ UserUtility.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace advtech.Finance.Accounta
+{
+    public class UserUtility
+    {
+        public string UserName { get; set; }
+        public UserUtility() { }
+        public string BindUser()
+        {
+
+            String CS = ConfigurationManager.ConnectionStrings["MyDatabaseConnectionString1"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(CS))
+            {
+                con.Open();
+                SqlCommand cmd2AC = new SqlCommand("select * from Users where Username='" + System.Web.HttpContext.Current.Session["USERNAME"].ToString() + "'", con);
+                SqlDataReader readerAC = cmd2AC.ExecuteReader();
+
+                if (readerAC.Read())
+                {
+                    String FN = readerAC["Name"].ToString();
+                    readerAC.Close();
+                    UserName = FN;
+                }
+            }
+            return UserName;
+        }
+    }
+}
